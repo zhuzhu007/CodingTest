d4c23668bfad9e0991df3f07dcd9bfd7032e9041
zhuzhu007 <47916316+zhuzhu007@users.noreply.github.com>
2019-02-24 12:03:13 +1100

    Update CountingRule2.cs

--- CodingTest/CountingRule2.cs
+++ CodingTest/CountingRule2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -15,37 +15,22 @@ namespace CodingTest
         }
         public override int Count(List<string> words)
         {
-            if (Filters.Any() && Filters.Count == 2 && words.Any())
-            {
-                var filter1 = Filters[0];
-                var filter2 = Filters[1];
-                if (filter1.Values.Any() && filter2.Values.Any())
-                {
-                    var startWithbOrB = words.Where(x => filter1.Values.Any(y => x.StartsWith(y.ToString()))).ToList();
-                    return startWithbOrB.Sum(x => x.Count(y => filter2.Values.Contains(y)));
-                }
-            }
+            if (words == null || !words.Any())
+                return -1;
+            var filter1 = Filters[0];
+            var filter2 = Filters[1];
+            var startWithbOrB = words.Where(x => filter1.Values.Any(y => x.StartsWith(y.ToString()))).ToList();
+            return startWithbOrB.Sum(x => x.Count(y => filter2.Values.Contains(y)));
 
-            //if no filters or words
-            return -1;
         }
 
         public override string GetRuleName()
         {
-            if (Filters.Any() && Filters.Count == 2)
-            {
-                var filter1 = Filters[0];
-                var filter2 = Filters[1];
-                if (!string.IsNullOrEmpty(filter1.Name) && filter1.Values.Any() && !string.IsNullOrEmpty(filter2.Name) && filter2.Values.Any())
-                {
-                    var lowcases1 = FilterOutUpperCase(filter1.Values);
-                    var lowcases2 = FilterOutUpperCase(filter2.Values);
-                    return $"{filter2.Name}_{string.Join("", lowcases2)}_in_words_{filter1.Name}_{string.Join("", lowcases1)}";
-                }
-
-            }
-
-            return "something wrong in rule2's filter, cannot get the name";
+            var filter1 = Filters[0];
+            var filter2 = Filters[1];
+            var lowcases1 = FilterOutUpperCase(filter1.Values);
+            var lowcases2 = FilterOutUpperCase(filter2.Values);
+            return $"{filter2.Name}_{string.Join("", lowcases2)}_in_words_{filter1.Name}_{string.Join("", lowcases1)}";
         }
     }
 }
